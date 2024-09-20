36bcd835e2a66f4d8781de400dcda93178123e45
ealahd <eelis.lahde@gmail.com>
2024-09-20 18:23:11 +0300

    .

--- src/test/java/com/potdeposit/PotDepositTest.java
+++ src/test/java/com/potdeposit/PotDepositTest.java
@@ -3,11 +3,11 @@ package com.potdeposit;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PotDepositTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PotDeposit.class);
 		RuneLite.main(args);
 	}
 }
